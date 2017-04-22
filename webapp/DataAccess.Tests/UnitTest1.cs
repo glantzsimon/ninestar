381354ddb684cb9516b7c107c1b7d2fcedba781a
Simon <si_glantz@yahoo.co.uk>
2017-04-22 20:25:10 +0100

    sg added entity framework and dapper

---
+++ webapp/DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccess.Tests
+{
+	/// <summary>
+	/// Summary description for UnitTest1
+	/// </summary>
+	[TestClass]
+	public class UnitTest1
+	{
+		public UnitTest1()
+		{
+			
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			//
+			// TODO: Add test logic here
+			//
+		}
+	}
+}

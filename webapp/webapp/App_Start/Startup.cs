77f64efa3640c1c50a7147ef71007c656338083a
Simon <si_glantz@yahoo.co.uk>
2017-04-22 19:06:58 +0100

    sg added webbapp

---
+++ webapp/webapp/App_Start/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Autofac;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+
+namespace webapp
+{
+	public static class Starup
+	{
+		public static void Register()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterControllers(typeof(MvcApplication).Assembly);
+			builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+			builder.RegisterModelBinderProvider();
+			builder.RegisterModule<AutofacWebTypesModule>();
+			builder.RegisterSource(new ViewRegistrationSource());
+			builder.RegisterFilterProvider();
+
+			var container = builder.Build();
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+		}
+	}
+}

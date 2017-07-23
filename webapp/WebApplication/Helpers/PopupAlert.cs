ea762f21c2655165969ca28da33afe7e93dcfb4b
Simon <si_glantz@yahoo.co.uk>
2017-07-23 20:35:23 +0100

    sg - fixed popup alert - minimal markup

--- webapp/WebApplication/Helpers/PopupAlert.cs
+++ webapp/WebApplication/Helpers/PopupAlert.cs
@@ -34,6 +34,18 @@ namespace K9.WebApplication.Helpers
 			});
 		}
 
+		public static MvcHtmlString PopupAlert(this HtmlHelper html)
+		{
+			if (html.ViewBag.IsPopuAlert != null)
+			{
+				if ((bool)html.ViewBag.IsPopuAlert)
+				{
+					return html.Partial("_PopupAlert", html.ViewBag.AlertOptions as AlertOptions);		
+				}
+			}
+			return MvcHtmlString.Empty;
+		}
+
 		public static MvcHtmlString PopupAlert(this HtmlHelper html, AlertOptions options)
 		{
 			return html.Partial("_PopupAlert", options);

af74e7687a84f4e5d5e22c106ff72258feff779b
Simon <si_glantz@yahoo.co.uk>
2017-07-29 16:35:46 +0100

    sg - added link to full size image on caoursel

--- webapp/WebApplication/Options/CarouselOptions.cs
+++ webapp/WebApplication/Options/CarouselOptions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using K9.SharedLibrary.Models;
 using K9.WebApplication.Helpers;
 
@@ -10,12 +12,15 @@ namespace K9.WebApplication.Options
 	{
 
 		private readonly string _pathToImages;
+		private readonly string _pathToFullSizeImages;
 		private List<IAssetInfo> _images;
+		private List<IAssetInfo> _fullSizeImages;
 		private static Guid _id = Guid.NewGuid();
 
-		public CarouselOptions(string pathToImages)
+		public CarouselOptions(string pathToImages, string fullSizeImageFolderName  = "full-size")
 		{
 			_pathToImages = pathToImages;
+			_pathToFullSizeImages = Path.Combine(_pathToImages, fullSizeImageFolderName);
 			LoadImages();
 		}
 
@@ -34,9 +39,20 @@ namespace K9.WebApplication.Options
 			get { return _images; }
 		}
 
+		public List<IAssetInfo> FullSizeImages
+		{
+			get { return _fullSizeImages; }
+		}
+
+		public IAssetInfo GetFullSizeImage(string imageName)
+		{
+			return _fullSizeImages.FirstOrDefault(i => i.FileName == imageName);
+		}
+
 		private void LoadImages()
 		{
-			_images = ContentHelper.GetImageFiles(PathToImages);
+			_images = ContentHelper.GetImageFiles(_pathToImages);
+			_fullSizeImages = ContentHelper.GetImageFiles(_pathToFullSizeImages);
 		}
 
 	}

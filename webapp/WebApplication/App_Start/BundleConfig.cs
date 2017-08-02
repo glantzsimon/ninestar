40d5cfe6975c57f1c48ad9fbb68a58e76bbb9166
Simon <si_glantz@yahoo.co.uk>
2017-08-03 00:14:39 +0100

    sg - massaged css further

--- webapp/WebApplication/App_Start/BundleConfig.cs
+++ webapp/WebApplication/App_Start/BundleConfig.cs
@@ -22,7 +22,19 @@ namespace K9.WebApplication
 				"~/Content/botf.css"));
 
 			bundles.Add(new StyleBundle("~/Content/tpl").Include(
-				"~/Content/template/*.css"));
+				"~/Content/template/lsb.css",
+				"~/Content/template/owl.carousel.css",
+				"~/Content/template/owl.theme.css",
+				"~/Content/template/style.css",
+				"~/Content/template/style.1080.css",
+				"~/Content/template/style.1024.css",
+				"~/Content/template/style.991.css",
+				"~/Content/template/style.736.css",
+				"~/Content/template/style.480.css",
+				"~/Content/template/style.414.css",
+				"~/Content/template/style.384.css",
+				"~/Content/template/style.375.css",
+				"~/Content/template/style.320.css"));
 
 			bundles.Add(new ScriptBundle("~/Scripts/js").Include(
 				"~/Scripts/imageSwitcher/*.js",

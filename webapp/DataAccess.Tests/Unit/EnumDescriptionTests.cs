716ea047e1c27f885fc082a88c51e17ecf14dd29
Simon <si_glantz@yahoo.co.uk>
2017-08-10 20:34:17 +0100

    sg - added GetLanguageCode and tests

--- webapp/DataAccess.Tests/Unit/EnumDescriptionTests.cs
+++ webapp/DataAccess.Tests/Unit/EnumDescriptionTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Globalization;
+using System.Threading;
 using K9.DataAccess.Enums;
 using K9.DataAccess.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,9 +14,27 @@ namespace K9.DataAccess.Tests.Unit
 		[TestMethod]
 		public void ELanguage_GetLanguageDescription_ShouldReturnCorrectLanguage()
 		{
-			var language = ELanguage.English.GetLocalisedLanguageName();
+			var english = ELanguage.English.GetLocalisedLanguageName();
+			var french = ELanguage.French.GetLocalisedLanguageName();
+
+			Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr");
+			var anglais = ELanguage.English.GetLocalisedLanguageName();
+			var francais = ELanguage.French.GetLocalisedLanguageName();
 			
-			Assert.AreEqual("English", language);
+			Assert.AreEqual("English", english);
+			Assert.AreEqual("Anglais", anglais);
+			Assert.AreEqual("French", french);
+			Assert.AreEqual("Français", francais);
+		}
+
+		[TestMethod]
+		public void ELanguage_GetLanguageCode_ShouldReturnCorrectLanguageCode()
+		{
+			var languageCode = ELanguage.English.GetLanguageCode();
+			var languageCodeFr = ELanguage.French.GetLanguageCode();
+			
+			Assert.AreEqual("en", languageCode);
+			Assert.AreEqual("fr", languageCodeFr);
 		}
 
 	}

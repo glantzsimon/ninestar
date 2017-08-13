b5a26f0fa0c75773df0ee5b27a63915e90205de3
Simon <si_glantz@yahoo.co.uk>
2017-08-13 02:38:38 +0100

    sg - added remove file function

--- webapp/SharedLibrary/Helpers/FileSourceHelper.cs
+++ webapp/SharedLibrary/Helpers/FileSourceHelper.cs
@@ -1,32 +1,21 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using K9.SharedLibrary.Extensions;
 using K9.SharedLibrary.Models;
+using NLog;
 
 namespace K9.SharedLibrary.Helpers
 {
 	public class FileSourceHelper : IFileSourceHelper
 	{
 		private readonly IPostedFileHelper _postedFileHelper;
+		private readonly ILogger _logger;
 
-		public FileSourceHelper(IPostedFileHelper postedFileHelper)
+		public FileSourceHelper(IPostedFileHelper postedFileHelper, ILogger logger)
 		{
 			_postedFileHelper = postedFileHelper;
-		}
-
-		public void SaveFilesToDisk(FileSource fileSource, bool createDirectory = false)
-		{
-			if (!Directory.Exists(fileSource.PathToFiles))
-			{
-				if (createDirectory)
-				{
-					CreateDirectory(fileSource);
-				}
-			}
-			foreach (var httpPostedFileBase in fileSource.PostedFile)
-			{
-				_postedFileHelper.SavePostedFileToRelativePath(httpPostedFileBase, fileSource.PathToFiles);
-			}
+			_logger = logger;
 		}
 
 		public void LoadFiles(FileSource fileSource, bool throwErrorIfDirectoryNotFound = true)
@@ -41,12 +30,50 @@ namespace K9.SharedLibrary.Helpers
 				catch (Exception ex)
 				{
 					throw new Exception("An error occurred whilst trying to load the files.", ex);
-				}	
+				}
 			}
 			else if (throwErrorIfDirectoryNotFound)
 			{
 				throw new DirectoryNotFoundException(string.Format("The directory {0} does not exist.", pathOnDisk));
 			}
+		}
+
+		public void SaveFilesToDisk(FileSource fileSource, bool createDirectory = false)
+		{
+			if (!Directory.Exists(fileSource.PathToFiles))
+			{
+				if (createDirectory)
+				{
+					CreateDirectory(fileSource);
+				}
+			}
+			SavePostedFiles(fileSource);
+			UpdateUploadedFiles(fileSource);
+		}
+
+		private void SavePostedFiles(FileSource fileSource)
+		{
+			foreach (var httpPostedFileBase in fileSource.PostedFile)
+			{
+				_postedFileHelper.SavePostedFileToRelativePath(httpPostedFileBase, fileSource.PathToFiles);
+			}
+		}
+
+		private void UpdateUploadedFiles(FileSource fileSource)
+		{
+			var filesToDelete = ContentHelper.GetFiles(fileSource.PathToFiles)
+				.Where(f => !fileSource.UploadedFiles.Select(a => a.FileName).Contains(f.FileName)).ToList();
+			filesToDelete.ForEach(f =>
+			{
+				try
+				{
+					File.Delete(f.PathOnDisk);
+				}
+				catch (Exception ex)
+				{
+					_logger.Error("UpdateUploadedFiles => could not delete file {0}. {1}", f.FileName, ex.Message);
+				}
+			});
 		}
 
 		private void CreateDirectory(FileSource fileSource)

504c716740dbef7ef1ecc8a95b0f1e4e4ef73545
Simon Glantz <simon.glantz@mac.com>
2017-09-10 03:59:02 +0100

    sg - added build.ps1

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -1,3 +1,71 @@
-param([String]$key='')
+$appName = "ninestar"
+$publishDir = "publish"
+$appDir = "../webapp"
+$testDir = "../webapp/WebApplication.Tests"
 
-git-crypt unlock $key
+function ProcessErrors(){
+  if($? -eq $false)
+  {
+    throw "The previous command failed (see above)";
+  }
+}
+
+function _DeleteFile($fileName) {
+  If (Test-Path $fileName) {
+    Write-Host "Deleting '$fileName'"
+    Remove-Item $fileName
+  } else {
+    "'$fileName' not found. Nothing deleted"
+  }
+}
+
+function _Clean() {
+  echo "Cleaning old content"
+
+  _DeleteFile "$appName.zip"
+}
+
+function NugetRestore() {
+  echo "Running nuget restore"
+
+  pushd $appDir
+  nuget restore
+  ProcessErrors
+}
+
+function _DotnetTest() {
+  echo "Running dotnet test"
+  pushd $testDir
+  ProcessErrors
+
+  dotnet test
+  ProcessErrors
+
+  popd
+}
+
+function _Build() {
+  echo "Building App"
+  pushd $appPath
+  ProcessErrors
+
+  Msbuild -p Configuration=Release -p DeployOnBuild=true -p PublishProfile=IntegrationLocal
+  ProcessErrors
+
+  popd
+}
+
+function Main {
+  Try {
+    _Clean
+    _NugetRestore
+    _Test
+    _Build
+  }
+  Catch {
+    Write-Error $_.Exception
+    exit 1
+  }
+}
+
+Main

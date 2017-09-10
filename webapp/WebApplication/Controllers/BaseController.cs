60551242e4b68d2a0b27b8a205b754311463d302
Simon Glantz <simon.glantz@mac.com>
2017-09-10 22:15:03 +0100

    sg - removed net.http from base controller

--- webapp/WebApplication/Controllers/BaseController.cs
+++ webapp/WebApplication/Controllers/BaseController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading;
-using System.Web.Http;
 using System.Web.Mvc;
 using K9.DataAccess.Models;
 using K9.Globalisation;

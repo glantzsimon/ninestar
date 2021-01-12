5b3a60cf92c9e4690513c6c0bf26c9ea71a4b286
= <=>
2021-01-12 14:32:48 +0700

    sg - update

--- webapp/WebApplication/Controllers/ConsultationsController.cs
+++ webapp/WebApplication/Controllers/ConsultationsController.cs
@@ -3,6 +3,7 @@ using K9.Base.WebApplication.Filters;
 using K9.Base.WebApplication.UnitsOfWork;
 using K9.DataAccessLayer.Models;
 using K9.SharedLibrary.Authentication;
+using K9.SharedLibrary.Models;
 using System.Web.Mvc;
 
 namespace K9.WebApplication.Controllers
@@ -11,10 +12,19 @@ namespace K9.WebApplication.Controllers
     [RequirePermissions(Role = RoleNames.Administrators)]
     public class ConsultationsController : BaseController<Consultation>
     {
-        public ConsultationsController(IControllerPackage<Consultation> controllerPackage)
+        private readonly IRepository<Contact> _contactRepository;
+
+        public ConsultationsController(IControllerPackage<Consultation> controllerPackage, IRepository<Contact> contactRepository)
             : base(controllerPackage)
         {
+            _contactRepository = contactRepository;
+            RecordBeforeDetails += ConsultationsController_RecordBeforeDetails;
+        }
 
+        private void ConsultationsController_RecordBeforeDetails(object sender, Base.WebApplication.EventArgs.CrudEventArgs e)
+        {
+            var consultation = e.Item as Consultation;
+            consultation.Contact = _contactRepository.Find(consultation.ContactId);
         }
     }
 }

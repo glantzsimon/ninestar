1ec64bb2b2676e5b6fc419f09ab775a82c94ce3a
= <=>
2021-01-14 04:44:52 +0700

    sg - update

--- webapp/WebApplication/Models/CompatibilityDetailsModel.cs
+++ webapp/WebApplication/Models/CompatibilityDetailsModel.cs
@@ -300,12 +300,12 @@ namespace K9.WebApplication.Models
                 if (transformationType == transformationType1)
                 {
                     sb.AppendLine(
-                        $"<h5>{{Person1Proper}}'s {type1} element '{energy1.ElementName}' {verb} {{Person2}}'s {type2} element '{energy2.ElementName}'</h5>.");
+                        $"<h5>{{Person1Proper}}'s {type1} element '{energy1.ElementName}' {verb} {{Person2}}'s {type2} element '{energy2.ElementName}'</h5>");
                 }
                 else if (transformationType == transformationType2)
                 {
                     sb.AppendLine(
-                        $"<h5>{{Person2Proper}}'s {type2} element '{energy2.ElementName}' {verb} {{Person1}}'s {type1} element '{energy1.ElementName}'</h5>.");
+                        $"<h5>{{Person2Proper}}'s {type2} element '{energy2.ElementName}' {verb} {{Person1}}'s {type1} element '{energy1.ElementName}'</h5>");
                 }
 
                 if (sb.Length > 0)

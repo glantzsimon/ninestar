e3a0b0326b833ec270ce85d2a463f1e4a9ae9eef
= <=>
2021-01-14 03:16:18 +0700

    sg - update

--- webapp/WebApplication/Options/GaugeOptions.cs
+++ webapp/WebApplication/Options/GaugeOptions.cs
@@ -4,6 +4,7 @@ namespace K9.WebApplication.Options
 {
     public class GaugeOptions
     {
+        public string Title { get; set; }
         public int Value { get; set; }
         public int MaxValue { get; set; } = (int)ECompatibilityScore.ExtremelyHigh;
         public bool IsInverted { get; set; }

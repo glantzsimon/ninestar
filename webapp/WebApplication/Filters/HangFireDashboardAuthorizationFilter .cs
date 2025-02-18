f447586ad54f9a015fea9e6ee091f3e3fe4df422
= <=>
2025-02-18 22:41:27 +0800

    sg - update

--- webapp/WebApplication/Filters/HangFireDashboardAuthorizationFilter .cs
+++ webapp/WebApplication/Filters/HangFireDashboardAuthorizationFilter .cs
@@ -1,17 +1,14 @@
-﻿using K9.SharedLibrary.Authentication;
+﻿using K9.WebApplication.Helpers;
 
 namespace K9.WebApplication.Services
 {
     using Hangfire.Dashboard;
-    using Microsoft.Owin;
 
     public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilter
     {
         public bool Authorize(DashboardContext context)
         {
-            var owinContext = new OwinContext(context.GetOwinEnvironment());
-            var user = owinContext.Authentication.User;
-            return user?.Identity?.IsAuthenticated == true && user.IsInRole(RoleNames.Administrators);
+            return SessionHelper.CurrentUserIsAdmin();
         }
     }
 }

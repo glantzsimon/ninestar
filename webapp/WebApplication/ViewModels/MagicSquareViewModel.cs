6c4ed471103c8a6636531e691c5563c97e654cee
= <=>
2025-03-30 18:41:51 +0800

    sg - update

---
+++ webapp/WebApplication/ViewModels/MagicSquareViewModel.cs
@@ -0,0 +1,21 @@
+﻿using K9.WebApplication.Models;
+
+namespace K9.WebApplication.ViewModels
+{
+    public class MagicSquareViewModel
+    {
+        public NineStarKiEnergy GlobalEnergy { get; set; }
+        public NineStarKiEnergy HouseOccupiedEnergy { get; set; }
+
+        public NineStarKiDirections GetDirections()
+        {
+            var oppositeHouseOfFiveEnergy = new NineStarKiEnergy((ENineStarKiEnergy)NineStarKiModel.GetOppositeEnergyInMagicSquare(GlobalEnergy.HouseOfFive.EnergyNumber));
+            var oppositeHouseOccupiedEnergy = new NineStarKiEnergy((ENineStarKiEnergy)NineStarKiModel.GetOppositeEnergyInMagicSquare(HouseOccupiedEnergy.EnergyNumber));
+
+            return new NineStarKiDirections(GlobalEnergy.HouseOfFive.Direction,
+               oppositeHouseOfFiveEnergy.Direction,
+                HouseOccupiedEnergy.Direction,
+                oppositeHouseOccupiedEnergy.Direction);
+        }
+    }
+}

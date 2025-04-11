ae915eed2e167ede2dc4a979f2c50bbe2a24a054
= <=>
2025-04-11 17:18:34 +0800

    sg - update

--- webapp/WebApplication/App_Start/RelativeCssRewriteUrlTransform.cs
+++ webapp/WebApplication/App_Start/RelativeCssRewriteUrlTransform.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Optimization;
 
 namespace K9.WebApplication.Helpers
@@ -7,12 +9,28 @@ namespace K9.WebApplication.Helpers
     {
         public string Process(string includedVirtualPath, string input)
         {
-            var basePath = VirtualPathUtility.ToAbsolute("~");
             var rewrite = new CssRewriteUrlTransform();
-            var rewritten = rewrite.Process(includedVirtualPath, input);
+            var appPath = VirtualPathUtility.ToAbsolute("~").TrimEnd('/');
 
-            // Prepend virtual path if missing
-            return rewritten.Replace("url(/Content", $"url({basePath.TrimEnd('/')}/Content");
+            return Regex.Replace(input, @"url\((['""]?)([^)'""]+)\1\)", match =>
+            {
+                var quote = match.Groups[1].Value;
+                var url = match.Groups[2].Value;
+
+                // Skip fully qualified external URLs
+                if (url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ||
+                    url.StartsWith("https://", StringComparison.OrdinalIgnoreCase) ||
+                    url.StartsWith("//"))
+                {
+                    return match.Value; // Don't touch it
+                }
+
+                // Let the standard rewrite handle the path
+                var rewritten = rewrite.Process(includedVirtualPath, match.Value);
+
+                // If the rewritten URL starts with /Content (i.e., app-relative), prepend app base path
+                return Regex.Replace(rewritten, @"url\((['""]?)/Content", $"url($1{appPath}/Content", RegexOptions.IgnoreCase);
+            }, RegexOptions.IgnoreCase);
         }
     }
 }

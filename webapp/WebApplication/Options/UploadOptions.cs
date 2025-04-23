741678faa9d86ece9a645aa8418572a881917bb2
= <=>
2025-04-23 14:18:34 +0800

    sg - update

--- webapp/WebApplication/Options/UploadOptions.cs
+++ webapp/WebApplication/Options/UploadOptions.cs
@@ -6,6 +6,8 @@ namespace K9.WebApplication.Options
     public class UploadOptions
     {
         public string UploadUrl { get; set; }
+        public int MaxUploads { get; set; }
+        public int UploadCount { get; set; }
         public string IdPrefix { get; set; } = Guid.NewGuid().ToString("N");
         public EFilesSourceFilter Filter { get; set; }
         public string Accept => GetFilterString();

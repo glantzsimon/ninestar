e4a2f0bdb8f717ec28a0c41d19850cf5aef2d269
= <=>
2025-05-01 22:43:29 +0800

    sg - update

--- webapp/WebApplication/ViewModels/ConsultationBookingViewModel.cs
+++ webapp/WebApplication/ViewModels/ConsultationBookingViewModel.cs
@@ -11,11 +11,11 @@ namespace K9.WebApplication.ViewModels
         public Slot Slot { get; set; }
 
         public int? Id => Slot?.Id;
-        public string Title => Slot.IsTaken ? UserConsultation?.User.FullName : Dictionary.Available;
+        public string Title => Slot.IsTaken ? UserConsultation?.User.FullName : IsFree ? Dictionary.Available : Dictionary.Unavailable;
         public DateTime? StartsOn => Slot?.StartsOnLocalTime.Value.DateTime;
         public DateTime? EndsOn => Slot?.EndsOnLocalTime.Value.DateTime;
         public bool IsTaken => Slot.IsTaken;
-        public bool IsFree => !IsTaken && StartsOn <= DateTime.UtcNow.ToLocalTime();
+        public bool IsFree => !IsTaken && StartsOn >= DateTime.UtcNow.ToLocalTime();
         public string Name => Slot.IsTaken ? UserConsultation?.User.FullName ?? "" : "";
     }
 }

3780414674abb364c5508ac71ee14307d129c67c
= <=>
2025-05-06 18:33:21 +0800

    sg - update

--- webapp/WebApplication/Models/NineStarKi/PersonModel.cs
+++ webapp/WebApplication/Models/NineStarKi/PersonModel.cs
@@ -13,6 +13,7 @@ namespace K9.WebApplication.Models
         [Display(ResourceType = typeof(Globalisation.Dictionary), Name = Globalisation.Strings.Labels.GenderLabel)]
         public EGender Gender { get; set; }
 
+        [UIHint("Date")]
         [Required(ErrorMessageResourceType = typeof(Dictionary), ErrorMessageResourceName = Strings.ErrorMessages.FieldIsRequired)]
         [Display(ResourceType = typeof(Globalisation.Dictionary), Name = Globalisation.Strings.Labels.DateOfBirthLabel)]
         public DateTime DateOfBirth { get; set; }
